cd68da7297094fbc8370f13e3b50b65732fe7511
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2025-05-21 23:08:15 +0530

    test key added

--- src/pages/BookingDetails.jsx
+++ src/pages/BookingDetails.jsx
@@ -7,7 +7,8 @@ import axios from 'axios';
 import AdditionalServices from './AdditionalServices';
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe("pk_live_51RL5KtKBPwsna3OvmvQ3EbqIz8rMbj9Aj26Ur4X7y086GWPCZlQL3IEApu4ujhyUEtewYW4Fs1hezG9QmxfEKJtW00ySCy7f2C"); // replace with your Stripe publishable key
+// const stripePromise = loadStripe("pk_live_51RL5KtKBPwsna3OvmvQ3EbqIz8rMbj9Aj26Ur4X7y086GWPCZlQL3IEApu4ujhyUEtewYW4Fs1hezG9QmxfEKJtW00ySCy7f2C"); // replace with your Stripe publishable key
+const stripePromise = loadStripe("pk_test_51RL5LKGfC2VzRjpzblhE80Xh5e2gJEDSQ345CBQoaFoMBLSwo0BVKLTYTLHAEAneSmaLs8yI2pBcGqEI3xTPZqFZ00pbEmjRoC"); // replace with your Stripe publishable key
 
 // List of UK cities for validation
 const UK_CITIES = [
@@ -285,8 +286,8 @@ const BookingDetails = () => {
         username: customerDetails.name || 'NA',
         email: customerDetails.email || 'NA',
         phoneNumber: customerDetails.phone || 'NA',
-        // price: totalPrice || 0,
-        price: 0,
+        price: totalPrice || 0,
+        // price: 0,
         distance: parseInt(journey.distance) || 0,
         route: "default route",
         duration: journey.duration || "N/A",
