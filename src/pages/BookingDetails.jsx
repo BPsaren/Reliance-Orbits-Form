20aa59fe1cb150bf421fc7e777ce852cb353eb49
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2025-05-22 18:58:13 +0530

    key hidden

--- src/pages/BookingDetails.jsx
+++ src/pages/BookingDetails.jsx
@@ -8,8 +8,8 @@ import AdditionalServices from './AdditionalServices';
 import { loadStripe } from '@stripe/stripe-js';
 
 // const stripePromise = loadStripe("pk_live_51RL5KtKBPwsna3OvmvQ3EbqIz8rMbj9Aj26Ur4X7y086GWPCZlQL3IEApu4ujhyUEtewYW4Fs1hezG9QmxfEKJtW00ySCy7f2C"); // replace with your Stripe publishable key
-const stripePromise = loadStripe("pk_test_51RL5LKGfC2VzRjpzblhE80Xh5e2gJEDSQ345CBQoaFoMBLSwo0BVKLTYTLHAEAneSmaLs8yI2pBcGqEI3xTPZqFZ00pbEmjRoC"); // replace with your Stripe publishable key
-
+// const stripePromise = loadStripe("pk_test_51RL5LKGfC2VzRjpzblhE80Xh5e2gJEDSQ345CBQoaFoMBLSwo0BVKLTYTLHAEAneSmaLs8yI2pBcGqEI3xTPZqFZ00pbEmjRoC"); // replace with your Stripe publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 // List of UK cities for validation
 const UK_CITIES = [
   "London", "Birmingham", "Manchester", "Leeds", "Sheffield",
