b675c67db6bc2091ad6f9b6db211731eb3eff7bb
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2025-06-06 04:02:24 +0530

    home rout changed for book

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/wp-content/uploads/react-app/',
-  base: '/wp-content/themes/your-theme/react-app/',
-  // base: '/',
+  // base: '/wp-content/themes/your-theme/react-app/',
+  base: '/',
   plugins: [react()],
 })
