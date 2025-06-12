5785e20fba89133b0551221a92bf4813baba6b5e
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2025-06-13 00:27:34 +0530

    Payment Integrated logged

--- src/pages/PaymentSuccess.jsx
+++ src/pages/PaymentSuccess.jsx
@@ -7,7 +7,7 @@ const PaymentSuccess = () => {
 
     const [searchParams] = useSearchParams();
     const sessionId = searchParams.get('session_id');
-    window.alert(sessionId);
+    // window.alert(sessionId);
     // const [local, setLocal] = useState('');
 
     const navigate = useNavigate();
